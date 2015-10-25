b8cb342b16e99f4d32c745806df80700954c01b1
Alonso Gutierrez <alonsougc@gmail.com>
2015-10-25 16:41:09 -0400

    Completed and Edited
    Finished editing output

--- cachematrix.R
+++ cachematrix.R
@@ -25,10 +25,9 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getinv()
         if(!is.null(m)){
-        	message("getting cached data")
+        	message("getting cached data.")
         	return(m)
         }
-        message("solving the mean")
         data <- x$get()
 		m <- solve(data)
         x$setinv(m)
