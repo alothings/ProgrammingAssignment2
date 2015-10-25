8a8cdcca28bd084837bd0c5c2ba0632cbfa28439
Alonso Gutierrez <alonsougc@gmail.com>
2015-10-25 16:40:01 -0400

    Completed hw
    Modified makeCacheMatrix and cacheSolve to output the inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## make a cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinv <- function(inver) m <<- inver
+	getinv <- function() m
+	list(set = set, get = get,
+		setinv = setinv,
+		getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Solve the mean for the matrix x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)){
+        	message("getting cached data")
+        	return(m)
+        }
+        message("solving the mean")
+        data <- x$get()
+		m <- solve(data)
+        x$setinv(m)
+        m
 }
